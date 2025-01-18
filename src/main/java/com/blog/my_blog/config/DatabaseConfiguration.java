d6a3431240215826fc8983f442f6596f5b062917
gabrielloli <bristotgl@gmail.com>
2025-01-17 23:19:22 -0300

    changes db from mysql to postgres

---
+++ src/main/java/com/blog/my_blog/config/DatabaseConfiguration.java
@@ -0,0 +1,51 @@
+package com.blog.my_blog.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfiguration {
+
+    @Value("${spring.datasource.url}")
+    String url;
+    @Value("${spring.datasource.username}")
+    String username;
+    @Value("${spring.datasource.password}")
+    String password;
+    @Value("${spring.datasource.driver-class-name}")
+    String driver;
+
+    //    @Bean
+    public DataSource basicDataSource() {
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setUrl(url);
+        ds.setUsername(username);
+        ds.setPassword(password);
+        ds.setDriverClassName(driver);
+        return ds;
+    }
+
+    @Bean
+    public DataSource hikariDataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setDriverClassName(driver);
+
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(1);
+        config.setPoolName("myblog-db-pool");
+        config.setMaxLifetime(600000); // 10 minutes
+        config.setConnectionTimeout(120000); // 2 minutes
+        config.setConnectionTestQuery("select 1");
+
+        return new HikariDataSource(config);
+    }
+}
